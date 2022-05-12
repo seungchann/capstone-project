e697c60cb349cbf001e8155d56f486c1ca6a3530
SChan-Kim <instufld@gmail.com>
2022-05-12 16:56:13 +0900

    feat: TaskCell layout

--- CapstoneProject/CapstoneProject/Views/TaskCell.swift
+++ CapstoneProject/CapstoneProject/Views/TaskCell.swift
@@ -23,9 +23,40 @@ public class TaskCell: UICollectionViewCell {
     public override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setLayout()
     }
     
     static func nib() -> UINib {
         return UINib(nibName: "TaskCell", bundle: nil)
     }
+    
+    func setLayout() {
+        self.addSubview(categoryColorBar)
+        self.addSubview(taskNameLabel)
+        self.addSubview(taskExpectedTimeLabel)
+        self.addSubview(taskBar)
+        
+        let topBottomMargin: CGFloat = 10
+        let middleMargin: CGFloat = (self.frame.height - (2 * topBottomMargin) - (taskNameLabel.frame.height + taskExpectedTimeLabel.frame.height + taskBar.frame.height)) / 2
+        
+        categoryColorBar.translatesAutoresizingMaskIntoConstraints = false
+        categoryColorBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30).isActive = true
+        categoryColorBar.topAnchor.constraint(equalTo: self.topAnchor, constant: 3).isActive = true
+        categoryColorBar.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 3).isActive = true
+        categoryColorBar.widthAnchor.constraint(equalToConstant: 3).isActive = true
+        
+        taskNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        taskNameLabel.leadingAnchor.constraint(equalTo: categoryColorBar.leadingAnchor, constant: 20).isActive = true
+        taskNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: topBottomMargin).isActive = true
+        
+        taskExpectedTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        taskExpectedTimeLabel.leadingAnchor.constraint(equalTo: taskNameLabel.leadingAnchor, constant: 0).isActive = true
+        taskExpectedTimeLabel.topAnchor.constraint(equalTo: taskNameLabel.bottomAnchor, constant: middleMargin).isActive = true
+        
+        taskBar.translatesAutoresizingMaskIntoConstraints = false
+        taskBar.leadingAnchor.constraint(equalTo: taskNameLabel.leadingAnchor, constant: 0).isActive = true
+        taskBar.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: topBottomMargin).isActive = true
+        taskBar.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        taskBar.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        }
 }

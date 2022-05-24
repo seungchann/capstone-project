44c638c483332997925432caec9a39640e0c19c9
SChan-Kim <instufld@gmail.com>
2022-05-25 02:16:28 +0900

    feat: 기한 날짜 입력 시 datePicker를 사용할 수 있도록 구현 feat: textField에서 키보드 활용하여 입력 시 view의 높이를 조절하여 내용이 가려지지 않도록 구현 feat: textField에서 return키 사용하여 키보드 dismiss하도록 구현 fix: touchesbegan 대신에 tapGesture 활용하여, 키보드 dismiss하도록 구현

--- CapstoneProject/CapstoneProject/Controllers/AddTaskViewController.swift
+++ CapstoneProject/CapstoneProject/Controllers/AddTaskViewController.swift
@@ -15,17 +15,21 @@ public class AddTaskViewController: UIViewController {
         return (view as! AddTaskView)
     }
     
+    let datePicker: UIDatePicker = UIDatePicker()
+    let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
     
     // MARK: - View Lifecycle
     public override func viewDidLoad() {
         super.viewDidLoad()
+        self.addTaskView.taskNameTextField.delegate = self
+        self.addTaskView.dateTextField.delegate = self
+        self.tapGesture.delegate = self
+        
         setupBackButton()
         setupAddTaskButton()
-    }
-    
-    // TextField에서 다른 화면 터치 시, 키보드 dismiss.
-    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.addTaskView.endEditing(true)
+        setKeyboardActions()
+        setStackViewsCorners()
+        createDataPickerView()
     }
     
     private func setupBackButton() {
@@ -41,5 +45,85 @@ public class AddTaskViewController: UIViewController {
             self.presentingViewController?.dismiss(animated: true)
         }
         self.addTaskView.addTaskButton.addAction(action, for: .touchUpInside)
+    }
+    
+    // TextField에서 다른 화면 터치하거나 return키 입력 시, 키보드 dismiss.
+    // keyboard가 나타날 때 textField를 가리지 않도록, view의 높이 변화시킴.
+    private func setKeyboardActions() {
+        self.view.addGestureRecognizer(self.tapGesture)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func setStackViewsCorners() {
+        self.addTaskView.stackFirstView.layer.cornerRadius = 15
+        self.addTaskView.stackSecondView.layer.cornerRadius = 15
+    }
+}
+
+extension AddTaskViewController: UITextFieldDelegate, UIGestureRecognizerDelegate {
+    
+    // TextField에서 다른 화면 터치 시, 키보드 dismiss.
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc
+    func keyboardWillShow(_ sender: Notification) {
+        self.addTaskView.addTaskButton.isHidden = true
+        self.view.frame.origin.y = 0
+        if let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            if keyboardHeight > 300 {
+                self.view.frame.origin.y = -200
+            } else {
+                self.view.frame.origin.y = -100
+            }
+        }
+    }
+    
+    @objc
+    func keyboardWillHide(_ sender: Notification) {
+        self.addTaskView.addTaskButton.isHidden = false
+        self.view.frame.origin.y = 0
+    }
+}
+
+extension AddTaskViewController {
+    
+    private func createDataPickerView() {
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 45))
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: #selector(cancelPressed))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        
+        toolbar.setItems([cancelButton, flexibleSpace, doneButton], animated: true)
+        self.addTaskView.dateTextField.inputAccessoryView = toolbar
+        self.addTaskView.dateTextField.inputView = datePicker
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .inline
+    }
+    
+    @objc
+    func donePressed() {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        self.addTaskView.dateTextField.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc
+    func cancelPressed() {
+        self.view.endEditing(true)
     }
 }
